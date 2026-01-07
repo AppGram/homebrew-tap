9a17a952c5334340d32c68867c47e7d258c9e278
Nedim <nedim@etera.tech>
2026-01-07 20:33:53 +0100

    Update agentnotch to v1.1.0

--- Casks/agentnotch.rb
+++ Casks/agentnotch.rb
@@ -1,6 +1,6 @@
 cask "agentnotch" do
-  version "1.0.0"
-  sha256 "ae1f3c89df6fa1b225743bc1a64117f2917fbd1410c1b53a54e83465a82693df"
+  version "1.1.0"
+  sha256 "4b524d5bb220c63c4f36bf496098358fcfa2884804cae10fd023a265d3f44f80"
 
   url "https://github.com/AppGram/agentnotch/releases/download/v#{version}/AgentNotch.zip"
   name "AgentNotch"
